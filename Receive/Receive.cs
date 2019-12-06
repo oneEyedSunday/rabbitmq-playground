5a549501a5923851a761b923993604044933bfc9
Idiakose O. Sunday <idiakosesunday@gmail.com>
2019-12-06 08:17:55 +0100

    Delcare receiver queue

--- Receive/Receive.cs
+++ Receive/Receive.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
 
 namespace Receive
 {
-    class Program
+    class Receive
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ConnectionFactory factory = new ConnectionFactory
+            {
+                HostName = "localhost"
+            };
+
+            using (IConnection connection = factory.CreateConnection())
+            using (IModel channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello", durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+            }
         }
     }
 }
