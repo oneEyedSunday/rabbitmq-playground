2a1dffc94f0a0675db87979106441095c38badb4
Idiakose O. Sunday <idiakosesunday@gmail.com>
2019-12-06 08:13:16 +0100

    Send messages

--- Send/Send.cs
+++ Send/Send.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using RabbitMQ.Client;
+using System.Text;
 
 namespace Send
 {
-    class Program
+    class Send
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using(var connection = factory.CreateConnection())
+            { // collapsable
+                using (var channel = connection.CreateModel())
+                {
+                    // channel.QueueDeclare("hello", false, false, false, null);
+                    // alternatively with named func args
+                    channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                    string message = "Hello from OneEyedSunday!!!";
+
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "", routingKey: "hello",
+                        basicProperties: null,
+                        body: body);
+
+                    Console.WriteLine(" [x] Sent {0} ", message);
+                }
+            }
+
+            Console.WriteLine(" Press [enter] to exit. ");
+
+            Console.ReadLine();
         }
     }
 }
