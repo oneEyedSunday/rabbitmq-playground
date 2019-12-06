4d38c396ca7b6828c61d8b5ca991bc15ebf435dd
Idiakose O. Sunday <idiakosesunday@gmail.com>
2019-12-06 08:25:12 +0100

    Asynchronous consumption

--- Receive/Receive.cs
+++ Receive/Receive.cs
@@ -21,6 +21,19 @@ namespace Receive
                     exclusive: false,
                     autoDelete: false,
                     arguments: null);
+
+                EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    string message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+                };
+
+                channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
             }
         }
     }
