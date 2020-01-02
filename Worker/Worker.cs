24337e1a4395dd106c800252a9b90f272a10bce8
Idiakose O. Sunday <idiakosesunday@gmail.com>
2020-01-02 06:10:40 +0100

    Manual ack

--- Worker/Worker.cs
+++ Worker/Worker.cs
@@ -32,9 +32,11 @@ namespace Worker
                 Computation.TimeConsuming(message.Split('.').Length - 1);
 
                 Console.WriteLine(" [x] Done");
+
+                channel.BasicAck(deliveryTag:ea.DeliveryTag, multiple:false);
             };
 
-            channel.BasicConsume(queue: "task_queue", autoAck: true, consumer: consumer);
+            channel.BasicConsume(queue: "task_queue", autoAck: false, consumer: consumer);
 
             Console.WriteLine(" Press [enter] to exit.");
             Console.ReadLine();
