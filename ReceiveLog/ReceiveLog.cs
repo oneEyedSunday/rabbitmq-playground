5b71182807f7cf7a6a19da7e0bc587ab676dc567
Idiakose O. Sunday <idiakosesunday@gmail.com>
2020-01-02 09:00:55 +0100

    Update gitignores

---
+++ ReceiveLog/ReceiveLog.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReceiveLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
