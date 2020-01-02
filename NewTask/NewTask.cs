4272ed4b62745a731d98c3300c95453914b25160
Idiakose O. Sunday <idiakosesunday@gmail.com>
2020-01-02 03:46:19 +0100

    Emit Tasks

---
+++ NewTask/NewTask.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace NewTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                string message = GetMessage(args);
+                byte[] body = Encoding.UTF8.GetBytes(message);
+
+                IBasicProperties properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
+
+                channel.BasicPublish(exchange: "",
+                    routingKey: "task_queue",
+                    basicProperties: properties,
+                    body: body
+                    );
+
+                Console.WriteLine(" [x] Sent {0} ", message);
+            }
+                    
+
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+        }
+    }
+}
