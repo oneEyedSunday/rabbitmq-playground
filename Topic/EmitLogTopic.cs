6053343ef9e7cfa54644b00bc5dbf4936168772e
Idiakose O. Sunday <idiakosesunday@gmail.com>
2020-01-21 12:12:08 +0100

    Topic exchanges

---
+++ Topic/EmitLogTopic.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+using System.Linq;
+
+namespace Topic
+{
+    class EmitLogTopic
+    {
+        static void Main(string[] args)
+        {
+            const string exchangeName = "topic_logs"; 
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: exchangeName, type: ExchangeType.Topic);
+                // 😢 why no !!(args.Length)
+                string routingKey = (args.Length > 0) ? args[0] : "anonymous.info";
+                string message = (args.Length > 1) ? string.Join(" ", args.Skip(1).ToArray()) : "Something just happened right now!!!";
+
+                byte[] body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: exchangeName, routingKey: routingKey, basicProperties: null, body: body);
+
+                Console.WriteLine(" [x] Sent '{0}':'{1}'", routingKey, message);
+            }
+        }
+    }
+}
---
+++ TopicReceive/ReceiveLogsTopic.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace TopicReceive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                const string exchangeName = "topic_logs";
+
+                channel.ExchangeDeclare(exchange: exchangeName, type: ExchangeType.Topic);
+                var queueName = channel.QueueDeclare().QueueName;
+
+                if (args.Length < 1)
+                {
+                    Console.Error.WriteLine("Usage: {0} [binding_key...]",
+                                        Environment.GetCommandLineArgs()[0]);
+
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                    Environment.ExitCode = 1;
+                    return;
+                }
+
+                foreach (string bindingKey in args)
+                {
+                    channel.QueueBind(queue: queueName, exchange: exchangeName,
+                                  routingKey: bindingKey);
+                }
+
+                Console.WriteLine(" [*] Waiting for messages. To exit press CTRL+C");
+
+                EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+
+                consumer.Received += (model, ea) =>
+                {
+                    byte[] body = ea.Body;
+                    string message = Encoding.UTF8.GetString(body);
+                    string routingKey = ea.RoutingKey;
+                    Console.WriteLine(" [x] Received from routing key: message >>> '{0}':'{1}'",routingKey, message);
+                };
+
+                channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
